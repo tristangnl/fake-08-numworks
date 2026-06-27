9997146e513059a1c251b53deb2c8b8278d06350
tristangnl <tristan.gnl@gmail.com>
2026-06-27 23:02:16 +0200

    better makefile but not finished, adopting an approach similar to the 3ds Makefile

--- platform/numworks/source/NumworksHost.cpp
+++ platform/numworks/source/NumworksHost.cpp
@@ -4,7 +4,7 @@
 #include "../../../source/hostVmShared.h"
 #include "../../../source/nibblehelpers.h"
 
-extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "FakeO8";
+extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "FAKEO8";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
 int main(int argc, char * argv[]) {
