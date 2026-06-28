d073281cb18f25f71e80365e78a2fabf63c5bbee
tristangnl <tristan.gnl@gmail.com>
2026-06-28 12:43:21 +0200

    fix icon.o and multiple def of main

--- platform/numworks/source/NumworksHost.cpp
+++ platform/numworks/source/NumworksHost.cpp
@@ -7,6 +7,3 @@
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "FAKEO8";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-int main(int argc, char * argv[]) {
-
-}
