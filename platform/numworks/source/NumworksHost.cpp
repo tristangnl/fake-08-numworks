4ce26039d14772ef8e6c937942d27f33eebcfbaa
tristangnl <tristan.gnl@gmail.com>
2026-06-29 17:40:00 +0200

    finally a working version on numworks: adapt makefile to nosys.specs, implement an empty host file

--- platform/numworks/source/NumworksHost.cpp
+++ platform/numworks/source/NumworksHost.cpp
@@ -1,5 +1,7 @@
 #include <eadk.h>
 
+using namespace std;
+
 #include "../../../source/host.h"
 #include "../../../source/hostVmShared.h"
 #include "../../../source/nibblehelpers.h"
@@ -7,3 +9,103 @@
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "FAKEO8";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
+float targetFrameTimeMs;
+
+// Required by the exception stack unwinder.
+// Unused on this platform.
+void *__exidx_start;
+void *__exidx_end;
+
+//for p8 cartridge
+const char *osd_getromdata(const char *name) {
+  return (char*)eadk_external_data;
+}
+
+Host::Host(int windowWidth, int windowHeight)  {
+
+ }
+
+
+void Host::oneTimeSetup(Audio* audio){
+
+}
+
+void Host::oneTimeCleanup(){
+
+}
+
+void Host::setTargetFps(int targetFps){
+    targetFrameTimeMs = 1000.0 / (float)targetFps;
+}
+
+void Host::forceStretch(StretchOption newStretch) {
+	
+}
+
+void Host::changeStretch(){
+    
+}
+
+bool Host::shouldFillAudioBuff(){
+    return false;
+}
+
+void* Host::getAudioBufferPointer(){
+    return NULL;
+}
+
+size_t Host::getAudioBufferSize(){
+    return 0;
+}
+
+void Host::playFilledAudioBuffer(){
+}
+
+bool Host::shouldRunMainLoop(){
+    return true;
+}
+
+InputState_t Host::scanInput(){
+    
+    return InputState_t {
+
+    };
+}
+
+bool Host::shouldQuit() {
+    
+    return false;
+}
+
+void Host::waitForTargetFps(){
+    
+}
+
+void Host::drawFrame(uint8_t* picoFb, uint8_t* screenPaletteMap, uint8_t drawMode){
+
+}
+
+vector<string> Host::listcarts(){
+    vector<string> carts;
+    
+    return carts;
+}
+
+const char* Host::logFilePrefix() {
+    return "";
+}
+
+std::string Host::customBiosLua() {
+    return "";
+}
+
+std::string Host::getCartDirectory() {
+    return "";
+}
+
+std::vector<std::string> Host::listdirs() {
+    std::vector<std::string> dirs;
+
+    
+    return dirs;
+}
